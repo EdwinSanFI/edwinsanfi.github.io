05bf8591580fe77bdc2cabbc37ce2bd60ad3ff1b
EdwinSanFI <jaret-@live.com.mx>
2021-01-27 20:42:59 -0600

    Archivos bases
    Subo todos estos archivos por la presentación de un proyecto

---
+++ script/calculadora_matrices.js
@@ -0,0 +1,18 @@
+function Resolver(filas, columnas)
+{
+    matriz[filas][columnas];
+    console.log(matriz);
+}
+var palabra = "Hola";
+var matriz = function (filas, columnas){
+
+};
+var filas = document.getElementById("filas");
+var columnas = document.getElementById("columnas");
+var boton = document.getElementById("resolver");
+if(boton)
+{
+    boton.addEventListener("click", Resolver(), true);
+}
+Resolver();
+console.log(matriz);
