676cc76273d861b5418e6ebacae2904dce8f1610
Edwin <jaret-@live.com.mx>
2021-01-28 10:03:35 -0600

    Quiero aprender a subirlo

---
+++ script/calculadora_matrices1.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+// var matriz = [
+//     [[espacio1_1], [espacio1_2], [espacio1_3]],
+//     [[espacio2_1], [espacio2_2], [espacio2_3]],
+//     [[espacio3_1], [espacio3_2], [espacio3_3]]
+// ]; 
+// console.log(matriz);
+// var espacio1_1 = document.getElementById("espacio1_1");
+// var espacio1_2 = document.getElementById("espacio1_2");
+// var espacio1_3 = document.getElementById("espacio1_3");
+// var espacio2_1 = document.getElementById("espacio2_1");
+// var espacio2_2 = document.getElementById("espacio2_2");
+// var espacio2_3 = document.getElementById("espacio2_3");
+// var espacio3_1 = document.getElementById("espacio3_1");
+// var espacio3_2 = document.getElementById("espacio3_2");
+// var espacio3_3 = document.getElementById("espacio3_3");
+// var boton = document.getElementById("Boton");
+// if(boton)
+// {
+//     boton.addEventListener("click", Resolver(), true);
+// }
+// function Resolver(matriz) {
+
+// }
+// Resolver();
+
+
+
+
+
+
+
+
+// class Fila {
+//         constructor (e1, e2, e3)
+//         {
+//             this.e1 = espacio1_1;
+//             this.e2 = espacio1_2
+//             this.e3 = espacio1_3;
+//         }
+// }
+
+// var matrizp = [];
+// matrizp.push( new Fila ([espacio1_1], [espacio1_2], [espacio1_3]));
+// matrizp.push( new Fila [[espacio2_1], [espacio2_2], [espacio2_3]]);
+// matrizp.push( new Fila [[espacio3_1], [espacio3_2], [espacio3_3]]);
